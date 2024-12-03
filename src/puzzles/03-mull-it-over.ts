24c8a1897d52697276bf3e74b6e7271af1b7fbf6
EepyBerry <eepyberry@proton.me>
2024-12-03 08:54:05 +0100

    Add day 3 code

---
+++ src/puzzles/03-mull-it-over.ts
@@ -0,0 +1,47 @@
+import { toAnswerString } from "../utils.ts";
+import { BasePuzzle } from "./base-puzzle.ts";
+
+export default class Puzzle3 extends BasePuzzle {
+
+  private memory: string = ''
+  private mulRgx = /mul\((\d*),(\d*)\)/gm
+  private conditionalMulRgx = /mul\((\d*),(\d*)\)|do\(\)|don't\(\)/gm
+
+  run(): void {
+    this.loadInput('03')
+    if (!this.input) return
+    this.setup()
+
+    console.log('\n   _____________________________________________________________________ ')
+    console.log(`  /                                                                     \\`)
+    console.log(`  ⎸      ★                  Day 3: Mull It Over                         ⎹`)
+    console.log('  ⎸                          _    ______    _               ★           ⎹')
+    console.log('  ⎸                         (_)  |______|  (_)      ★                   ⎹')
+    console.log(`  ⎸                                                                     ⎹`)
+    console.log(`  ⎸ mul() instructions total:    ${toAnswerString(this.partOne())} ⎹`)
+    console.log(`  ⎸ Nb of safe-ish reports:      ${toAnswerString(this.partTwo())} ⎹`)
+    console.log('  \\_____________________________________________________________________/')
+  }
+
+  protected setup(): void {
+    this.memory = this.input.join('')
+  }
+
+  protected partOne(): number {
+    return [...this.memory.matchAll(this.mulRgx)]
+      .map(instr => parseInt(instr[1] as string) * parseInt(instr[2] as string))
+      .reduce((cur, acc) => acc += cur, 0)
+  }
+
+  protected partTwo(): number {
+    let canMul = true
+    return [...this.memory.matchAll(this.conditionalMulRgx)]
+      .filter(instr => {
+        if (instr[0] === 'do()')          canMul = true
+        else if (instr[0] === 'don\'t()') canMul = false
+        return canMul && instr[0] !== 'do()'
+      })
+      .map(instr => parseInt(instr[1] as string) * parseInt(instr[2] as string))
+      .reduce((cur, acc) => acc += cur, 0)
+  }
+}
--- src/puzzles/index.ts
+++ src/puzzles/index.ts
@@ -1,7 +1,9 @@
 import Puzzle1 from "./01-historian-hysteria.ts";
 import Puzzle2 from "./02-red-nosed-reports.ts";
+import Puzzle3 from "./03-mull-it-over.ts";
 
 export default [
     new Puzzle1(),
-    new Puzzle2()
+    new Puzzle2(),
+    new Puzzle3()
 ]
