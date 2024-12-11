b3eefe52a7c4a074f0d1b9b7cdd2c059afb74e73
EepyBerry <eepyberry@proton.me>
2024-12-11 14:08:20 +0100

    Refactor day 11 for part 2 (WIP)

--- src/puzzles/11-plutonian-pebbles.ts
+++ src/puzzles/11-plutonian-pebbles.ts
@@ -1,9 +1,12 @@
-import { toAnswerString } from "@toolbox/utils";
+import { deepClone, toAnswerString } from "@toolbox/utils";
 import { BasePuzzle } from "./base-puzzle";
+
+type StoneChunk = { stones: number[] }
 
 export default class Puzzle11 extends BasePuzzle {
 
-  private stones: number[] = []
+  private stones: StoneChunk = { stones: [] }
+  private readonly CHUNK_SIZE = 8192
 
   run(): void {
     this.loadInput('11e')
@@ -18,19 +21,23 @@ export default class Puzzle11 extends BasePuzzle {
     console.log('  ⎸          ★               _    ______    _                           ⎹')
     console.log('  ⎸                         (_)  |______|  (_)                     ★    ⎹')
     console.log(`  ⎸                                                                     ⎹`)
-    console.log(`  ⎸ Nb of safe reports:          ${toAnswerString(this.partOne())} ⎹`)
-    console.log(`  ⎸ Nb of safe-ish reports:      ${toAnswerString(this.partTwo())} ⎹`)
+    console.log(`  ⎸ Total stones (25 blinks):    ${toAnswerString(this.partOne())} ⎹`)
+    console.log(`  ⎸ Total stones (75 blinks):    ${toAnswerString(this.partTwo())} ⎹`)
     console.log('  \\_____________________________________________________________________/')
   }
 
   protected setup(): void {
-    this.stones = this.input[0].split(' ').map(n => parseInt(n))
+    this.stones.stones.push(...this.input[0].split(' ').map(n => parseInt(n)))
     console.log(this.stones)
   }
 
   protected partOne(): number {
-    Array(6).fill(0).forEach(_ => this.blink())
-    return this.stones.length
+    let stonesCopy = deepClone(this.stones)
+    let total = 0
+    for (let c = 0; c < 25; c++) {
+      total += this.blink(stonesCopy)
+    }
+    return total
   }
 
   protected partTwo(): number {
@@ -39,22 +46,36 @@ export default class Puzzle11 extends BasePuzzle {
   
   // ----------------------------------------------------------------------------------------------
 
-  private blink() {
-    const stonesToAdd: number[][] = [] // [index, value]
-    let curStone: number, stoneNumStr: string
-    for (let i = 0; i < this.stones.length; i++) {
-      curStone = this.stones[i]
-      if (curStone === 0) this.stones[i] = 0
+  private blink(chunk: StoneChunk): number {
+    const newChunks: StoneChunk[] = [{ stones: [] }]
+    let curStone: number, splitStones: number[]
+    let chunkIdx = 0
+    for (let i = 0; i < chunk.stones.length; i++) {
+      curStone = chunk.stones[i]
+      if (curStone === 0) {
+        newChunks[chunkIdx].stones.push(1)
+      }
       else if (curStone.toString().length % 2 === 0) {
-        stoneNumStr = curStone.toString()
-        this.stones[i] = parseInt(stoneNumStr.slice(0, stoneNumStr.length/2))
-        stonesToAdd.push([i, parseInt(stoneNumStr.slice(0, stoneNumStr.length/2))])
+        splitStones = this.splitEvenNumber(curStone)
+        newChunks[chunkIdx].stones.push(splitStones[0])
+        newChunks[chunkIdx].stones.push(splitStones[1])
       }
       else {
-        this.stones[i] *= 1024
+        newChunks[chunkIdx].stones.push(curStone * 2024)
+      }
+
+      // add new chunk when current chunk is too large
+      if (newChunks[chunkIdx].stones.length >= this.CHUNK_SIZE) {
+        newChunks.push({ stones: [] })
+        chunkIdx++
       }
     }
-    stonesToAdd.forEach(sta => this.stones.splice(sta[0], 0, sta[1]))
-    console.log(this.stones.join(' '))
+    console.log(newChunks)
+    return newChunks.reduce((acc, cur) => acc += cur.stones.length, 0)
+  }
+
+  private splitEvenNumber(n: number): number[] {
+    let nstr = n.toString()
+    return [parseInt(nstr.substring(0, nstr.length/2)), parseInt(nstr.substring(nstr.length/2))]
   }
 }
